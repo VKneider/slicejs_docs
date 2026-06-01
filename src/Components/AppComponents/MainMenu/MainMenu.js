31b2fea43d7ecb73d28b242380ec20a3aa7d89f4
vkneider <victorkneider@gmail.com>
2026-06-01 00:00:44 -0400

    feat: enhance MainMenu with overlay functionality and improve styling

--- src/Components/AppComponents/MainMenu/MainMenu.js
+++ src/Components/AppComponents/MainMenu/MainMenu.js
@@ -6,6 +6,7 @@ export default class MainMenu extends HTMLElement {
       this.$menuButton = this.querySelector('.slice_menu_button');
       this.$closeButton = this.querySelector('.slice_close_menu');
       this.$menu = this.querySelector('.slice_menu');
+      this.$overlay = this.querySelector('.slice_menu_overlay');
 
       this.$menuButton.addEventListener('click', () => {
          this.handleOpenMenu();
@@ -13,17 +14,25 @@ export default class MainMenu extends HTMLElement {
       this.$closeButton.addEventListener('click', () => {
          this.handleCloseMenu();
       });
+      if (this.$overlay) {
+         this.$overlay.addEventListener('click', () => {
+            this.handleCloseMenu();
+         });
+      }
 
       slice.controller.setComponentProps(this, props);
       this.debuggerProps = [];
    }
 
    init() {
-      this.addEventListener('mouseleave', () => {
-         if (this.classList.contains('slice_menu_open')){
-            this.handleCloseMenu();
-         }
-      });
+      const canHover = window.matchMedia('(hover: hover) and (pointer: fine)').matches;
+      if (canHover) {
+         this.addEventListener('mouseleave', () => {
+            if (this.classList.contains('slice_menu_open')){
+               this.handleCloseMenu();
+            }
+         });
+      }
    }
 
    add(value) {
@@ -32,10 +41,16 @@ export default class MainMenu extends HTMLElement {
 
    handleOpenMenu() {
       this.classList.add('slice_menu_open');
+      if (this.$overlay) {
+         this.$overlay.classList.add('is-visible');
+      }
    }
 
    handleCloseMenu() {
       this.classList.remove('slice_menu_open');
+      if (this.$overlay) {
+         this.$overlay.classList.remove('is-visible');
+      }
    }
 }
 
